23712d087900f4ca47c8889c0989bfee4b730336
muinha <samuel_buinha@hotmail.com>
2019-03-21 10:30:34 -0300

    ADD Template Admin

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('site.index');
-});
+Route::get('/', 'SiteController@index');
+
+Auth::routes();
+
+Route::get('/admin', 'HomeController@index')->name('home');
